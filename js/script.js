a02f035d2cea649e501a8f1d2697982a7114f4c6
Ricardo Franco <ricardo.franco23@gmail.com>
2019-09-10 19:18:03 -0400

    Update script.js
    Most current version of the "script.js" file

--- js/script.js
+++ js/script.js
@@ -16,62 +16,56 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-// document.addEventListener('DOMContentLoaded', () => {
-   const studentItem = document.getElementsByClassName('student-item cf');
-   const itemsPerPage = 10;
-   const totalPages = Math.ceil(studentItem.length/itemsPerPage);
+
+const studentItem = document.getElementsByClassName('student-item cf');
+const itemsPerPage = 10;
+const totalPages = Math.ceil(studentItem.length/itemsPerPage);
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show. ***/
-   const showPage = (list, page) => {
-      let startIndex = (page * itemsPerPage) - itemsPerPage;
-      let endIndex = page * itemsPerPage;
-      for ( let i = 0; i < list.length; i++ ) {
-         if ( i >= startIndex && i < endIndex ) {
-            list[i].style.display = 'block';
-         } else {
-            list[i].style.display = 'none';
-         }
+const showPage = (list, page) => {
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   for ( let i = 0; i < list.length; i++ ) {
+      if ( i >= startIndex && i < endIndex ) {
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
       }
    }
+}
 
-
-   /*** Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
 ***/
+
 const appendPageLinks = (list) => {
    const paginationDiv = document.createElement('div');
    const pageDiv = document.querySelector('.page');
-   paginationDiv.className = "pagination";
-   pageDiv.appendChild(paginationDiv);
    const ul = document.createElement('ul');
-   paginationDiv.appendChild(ul);
    let li;
    let a;
+   paginationDiv.className = "pagination";
+   pageDiv.appendChild(paginationDiv);
+   paginationDiv.appendChild(ul);
    for (let i = 0; i < totalPages; i++) {
-     li = document.createElement('li');
-     a = document.createElement('a');
-     a.setAttribute("href", "#");
-     a.innerHTML = i + 1;
-     ul.appendChild(li);
-     li.appendChild(a);
+      li = document.createElement('li');
+      a = document.createElement('a');
+      a.setAttribute("href", "#");
+      a.innerHTML = i + 1;
+      ul.appendChild(li);
+      li.appendChild(a);
    }
    const myLinks = document.querySelectorAll('.pagination a');
    myLinks[0].className = "active";
-   for (let i = 0; i < totalPages; i++) {
-      a.addEventListener('click', (event) => {
-         for (let i = 0; i < totalPages; i++) {
-            myLinks[i].classList.remove('active');            
+   for (let i = 0; i < myLinks.length; i++ ) { 
+      myLinks[i].addEventListener('click', (event) => {  
+         for ( let j = 0; j < myLinks.length; j++ ) {
+            myLinks[j].classList.remove('active');
          }
-         let eventTarget = event.target;
+         const eventTarget = event.target;                  
          eventTarget.classList.add('active');
          showPage(list, eventTarget.innerHTML);
       });
@@ -81,18 +75,37 @@ const appendPageLinks = (list) => {
 showPage(studentItem, 1);
 appendPageLinks(studentItem);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// const search = () => {
+const parentDiv = document.querySelector('div.page-header');
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const searchButton = document.createElement('button');
+searchDiv.setAttribute("class", "student-search");
+searchInput.setAttribute("placeholder", "Search for students...");
+searchButton.innerHTML = "Search";
+parentDiv.appendChild(searchDiv);
+searchDiv.append(searchInput, searchButton);
 
+const performSearch = (input, names) => {
+   for ( let i = 0; i < names.length; i++) {
+      names[i].classList.remove('match');
+      if ( input.value.length !== 0 && names[i].textContent.toLowerCase().includes(input.value.toLowerCase()) ) {
+               names[i].classList.add('match');
+      }
+   }
+}
 
+searchButton.addEventListener('click', (event) => {
+   event.preventDefault();
+   performSearch(searchInput, studentItem);
+});
 
+searchInput.addEventListener('keyup', (event) => {
+   event.preventDefault();   
+   performSearch(searchInput, studentItem);  
+});
+// }
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-// });
 
 
 
